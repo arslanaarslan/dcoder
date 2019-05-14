1e04808e1d7817418c19ad40a2145d0066cdae9c
Arslan Arslan <arslanaarslan@gmail.com>
2019-05-14 18:28:03 +0300

    Array Decleration

---
+++ Java/Java_Array_Decleration.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+//Compiler version JDK 11.0.2
+
+class Dcoder
+{   
+    public static void main(String args[])
+    {   
+        // int[] numbers = new int[3] {1, 2, 3};                // Faulted decleration
+        char[] letters = new char[] {'A', 'B', 'C'};
+        // double[] doubles = new { 1.0d, 1.1d, 1.2d };         // Faulted decleration
+        String[] words = { "first", "second", "third" };
+    }
+}
